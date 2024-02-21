189c1f2fe8ef5b8a67f5b6ad0c5337e0285c2d4a
lusento <einohodov21@gmail.com>
2024-02-21 20:10:57 +0300

    Adding a promo

--- app/components/Promo/promo.jsx
+++ app/components/Promo/promo.jsx
@@ -1,5 +1,14 @@
+"use client";
+import { useState } from "react";
 import Styles from "./Promo.module.css"
+
 export const Promo = () => {
+  const [codeIsVisible, setCodeIsVisible] = useState(false);
+
+  const handleButtonClick = () => {
+    setCodeIsVisible(true);
+  }
+
   return (
     <section className={Styles["promo"]}>
       <div className={Styles["promo__description-block"]}>
@@ -7,7 +16,9 @@ export const Promo = () => {
         <p className={Styles["promo__description"]}>
           Скидка на все курсы Яндекс Практикума для пользователей нашего сайта!
         </p>
-        <button className={Styles.promoButton}>Получить код</button>
+        <button className={Styles["promoButton"]} onClick={handleButtonClick} disabled={codeIsVisible}>
+          {codeIsVisible ? <span className={Styles["promo-code"]}>WEBTEENS10</span> : 'Получить код'}
+        </button>
       </div>
       <img
         src="/images/promo-illustration.svg"
